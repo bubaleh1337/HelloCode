c9ad3a056eabd985536f63cc0956ed1085468aa1
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-02-09 23:02:52 +0500

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "nikita")
+{
+  Console.WriteLine("I love uuuuu!!! <3");
+}
+
+else
+{
+  Console.WriteLine("Who are you, " + username + "? I don't love you. Get out");
+}
